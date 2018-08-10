0a6671d10b1a2a21cf25a326903bd930660a0426
Michał Chęciński <michal.checinski@outlook.com>
2018-08-10 23:03:58 +0200

    Add process manipulation class

---
+++ src/mqttclient/Process.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace mqttclient
+{
+    public class Process
+    {
+        public static string IsRunning(string exename, string location)
+        {
+            try
+            {
+                bool isRunning = System.Diagnostics.Process.GetProcessesByName(exename)
+                                        .FirstOrDefault(p => p.MainModule.FileName.StartsWith(location)) != default(System.Diagnostics.Process);
+                if (isRunning == true)
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+            catch (Exception)
+            {
+                return "0";
+            }
+        }
+
+        public static string Close(string exename)
+        {
+            try
+            {
+                foreach (System.Diagnostics.Process proc in System.Diagnostics.Process.GetProcessesByName(exename))
+                {
+                    proc.Kill();
+                }
+                return "1";
+            }
+            catch (Exception)
+            {
+                return "0";
+            }
+        }
+    }
+}
